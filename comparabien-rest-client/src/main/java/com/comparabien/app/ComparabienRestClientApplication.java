d7ee4eca81abb8c8d7c80a5ab28077d2b9ff72ca
Miguel Cortegana <miguel.cortegana93@gmail.com>
2018-04-13 02:17:57 -0500

    Creacion de las carpetas de proyecto comparabien-rest-service y comparabien-rest-client / Adicion de archivo .gitignore para ignorar el seguimiento de ficheros propios del IDE Eclipse

---
+++ comparabien-rest-client/src/main/java/com/comparabien/app/ComparabienRestClientApplication.java
@@ -0,0 +1,12 @@
+package com.comparabien.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparabienRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparabienRestClientApplication.class, args);
+	}
+}
---
+++ comparabien-rest-service/src/main/java/com/comparabien/app/ComparabienRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.comparabien.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparabienRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparabienRestServiceApplication.class, args);
+	}
+}
