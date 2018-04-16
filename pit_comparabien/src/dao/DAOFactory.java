4cb7c777326d1abd21cf53d27deddd1131b82a0f
Miguel Cortegana <miguel.cortegana93@gmail.com>
2018-04-16 18:54:08 -0500

    Creacion de Carpeta de Proyecto
    Se creo la carpeta de proyecto y estructura basica.
    - Basado en Struts 2 y MyBatis
    - Se agrego estructura y paquetes basicos.
    - Clase DAOFactory
    - Clase Utilitarios

---
+++ pit_comparabien/src/dao/DAOFactory.java
@@ -0,0 +1,21 @@
+package dao;
+
+public abstract class DAOFactory {
+	// los posibles orígenes de datos
+	public static final int MYSQL = 1;
+	public static final int ORACLE = 2;
+	public static final int DB2 = 3;
+	public static final int SQLSERVER = 4;
+	public static final int XML = 5;
+	
+	
+
+	public static DAOFactory getDAOFactory(int whichFactory) {
+		switch (whichFactory) {
+		case MYSQL:
+			return new MySqlDAOFactory();
+		default:
+			return null;
+		}
+	}
+}
