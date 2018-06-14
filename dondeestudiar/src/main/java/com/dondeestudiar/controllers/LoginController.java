56617bdc62230c4fa0f2634ce7d915de22e00443
Miguel Cortegana <miguel.cortegana93@gmail.com>
2018-06-14 00:11:36 -0500

    Validación de login

--- dondeestudiar/src/main/java/com/dondeestudiar/controllers/LoginController.java
+++ dondeestudiar/src/main/java/com/dondeestudiar/controllers/LoginController.java
@@ -40,11 +40,11 @@ public class LoginController {
 		Usuario usuario = usuarioService.login( user.getUsuario(), user.getClave());
 		if( usuario == null  ) {
 			model.put("error", "Error: Usuario o Contraseña incorrecta");
-			return "/login";
+			return "login";
 		}else {
 			if( !usuario.isEstado() ) {
 				model.put("error", "Error: Su usuario no está habilitado para iniciar sesión");
-				return "/login";
+				return "login";
 			}else {
 				session.setAttribute("usuario", usuario);
 				model.put("titulo", "Principal");
